d85c0852f678c2ff854135b4ff478ccc587b2b89
1MiKHalyCH1 <bykoun@mail.ru>
2015-12-08 23:37:06 +0600

    Revert "fsdfds"
    This reverts commit 44279960ceb4fefb74df2e17c1366e7e1026ed32.

--- js/life.js
+++ js/life.js
@@ -1,29 +1,21 @@
-var num = 10;
-var matrix = [];
-var matrixNew = [];
-
-var width = 0; 
-var height = 0;
+var matrix = new Array(10);
+var matrixNew = new Array(10);
 
 function init() {
     var canvas = document.getElementById("life");
-    canvas.width = canvas.height = 600; 
+    canvas.width = canvas.height =  500; 
 	var context = canvas.getContext("2d");
+    
     
     canvas.addEventListener('click', function(evt) {
         var mousePos = getMousePos(canvas, evt);
-        newRect(canvas, mousePos);
+        newRect(canvas,mousePos);
       }, false);
     
-    widthAndHeightReset(canvas);
-    fieldDrawing(canvas);
-    matrixInit();
-}
+    num = 10; 
+    width = canvas.width/num; 
+    height = canvas.height/num;
 
-function fieldDrawing(canvas) {
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.strokeStyle = "#000";
     for (var i = 0; i <= num; i++)
     {
         context.moveTo(i * width, 0);
@@ -33,9 +25,7 @@ function fieldDrawing(canvas) {
         context.lineTo(canvas.width,i * height);
     }
     context.stroke();
-}
-
-function matrixInit() {
+    
     for (var i = 0; i < num; i++)
     {
         matrix[i] = [];
@@ -48,42 +38,30 @@ function matrixInit() {
     }
 }
 
-function widthAndHeightReset(canvas){
-    width = canvas.width / num; 
-    height = canvas.height / num;
-}
-
 function next_step() {
     var canvas = document.getElementById("life");
     var context = canvas.getContext("2d");
-    mainMech();
-    draw(canvas);
+    
+    
+    draw();
 }
 
-function cleaning() {
+function stop() {
     var canvas = document.getElementById("life");
     var context = canvas.getContext("2d");
-    matrixInit();
-    draw(canvas);
+    init();
 }
 
-function draw(canvas) {
+function draw() {
+    var canvas = document.getElementById("life");
     var context = canvas.getContext("2d");
+    
     for (var i = 0; i < num; i++)
         for (var j = 0; j < num; j++)
         {
-            drawRect(canvas,i,j);
+            if (matrix[i][j])
+                context.fillRect(i*50,j*50,50,50);
         }
-}
-
-function drawRect(canvas, x, y) {
-    var context = canvas.getContext("2d");
-    if (matrix[x][y])
-        context.fillRect(x*width+1,y*height+1,
-                           width-2, height-2);
-    else
-        context.clearRect(x*width+1,y*height+1,
-                           width-2, height-2);
 }
 
 function getMousePos(canvas, evt) {
@@ -96,59 +74,11 @@ function getMousePos(canvas, evt) {
 
 function newRect(canvas,mousePos) {
     var context = canvas.getContext("2d");
-    var x = Math.floor(mousePos.x/width);
-    var y = Math.floor(mousePos.y/height);
+    var x = Math.floor(mousePos.x/50);
+    var y = Math.floor(mousePos.y/50);
     matrix[x][y] = !matrix[x][y];
-    drawRect(canvas, x, y);
-}
-
-function mainMech() {
-    for (var i =  0; i < num; i++)
-        for (var j = 0; j < num; j++)
-        {
-            var localX = i - 1;
-            var localY = j - 1;
-            matrixNew[i][j] = liveOrDie(localX, localY);
-        }
-    
-    for (var i = 0; i < num; i++)
-        for (var j = 0; j < num; j++)
-            matrix[i][j] = matrixNew[i][j];
-}
-    
-function liveOrDie(x,y) {
-    var counter = 0;
-    for (var i = 0; i < 3; i++)
-        for (var j = 0; j < 3; j++)
-            if (matrix[realValue(x+i)][realValue(y+j)]) counter++;
-    if (matrix[x+1][y+1]) counter--;
-    return (counter == 3 || (matrix[x+1][y+1] && counter == 2));
-}
-
-function realValue(value) {
-    if (value == -1)
-        return num - 1;
-    if (value == num)
-        return 0; 
-    return value
-}
-
-function fieldPlus(){
-    var canvas = document.getElementById("life");
-    if (num != 70)
-        num+=10;
-    matrixInit();
-    widthAndHeightReset(canvas);
-    fieldDrawing(canvas);
-    draw(canvas);
-}
-
-function fieldMinus(){
-    var canvas = document.getElementById("life");
-    if (num != 10)
-        num-=10;
-    matrixInit();
-    widthAndHeightReset(canvas);
-    fieldDrawing(canvas);
-    draw(canvas);
+    if (matrix[x][y])
+        context.fillRect(x*50+1,y*50+1,48,48);
+    else
+        context.clearRect(x*50+1,y*50+1,48,48);
 }
